f9f4020950a2a296eb9ecb91ec78d1093e9376e1
Cecilia Mota <ceciliadias08@hotmail.com>
2025-10-28 20:26:28 -0400

    Fisrt commit

---
+++ src/main/java/ca/com/northpizza/north_pizza/NorthPizzaApplication.java
@@ -0,0 +1,13 @@
+package ca.com.northpizza.north_pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/com/northpizza/north_pizza/NorthPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.com.northpizza.north_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
